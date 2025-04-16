139ede51aeeafb4826c813d292275aa1056f19e1
AlanGregorio <105121970+lone23@users.noreply.github.com>
2025-04-16 10:57:49 +0200

    aggiunta node.js

--- 5_Applicativo/BELMOPOLY/vendor/composer/autoload_psr4.php
+++ 5_Applicativo/BELMOPOLY/vendor/composer/autoload_psr4.php
@@ -8,4 +8,9 @@ $baseDir = dirname($vendorDir);
 return array(
     'models\\' => array($baseDir . '/application/models'),
     'libs\\' => array($baseDir . '/application/libs'),
+    'WebSocket\\' => array($vendorDir . '/textalk/websocket/lib'),
+    'Psr\\Log\\' => array($vendorDir . '/psr/log/src'),
+    'Psr\\Http\\Message\\' => array($vendorDir . '/psr/http-factory/src', $vendorDir . '/psr/http-message/src'),
+    'Phrity\\Util\\' => array($vendorDir . '/phrity/util-errorhandler/src'),
+    'Phrity\\Net\\' => array($vendorDir . '/phrity/net-uri/src'),
 );
