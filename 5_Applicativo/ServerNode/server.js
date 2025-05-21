94f79157421d35c87de7bb2f492eaad95639c4ed
AlanGregorio <105121970+lone23@users.noreply.github.com>
2025-05-21 15:31:44 +0200

    Documentazione

--- 5_Applicativo/ServerNode/server.js
+++ 5_Applicativo/ServerNode/server.js
@@ -1,36 +1,31 @@
 const WebSocket = require('ws');
 const wss = new WebSocket.Server({ port: 3000 });
 
-let rooms = {}; // Oggetto per memorizzare le stanze e i loro client
+let rooms = {};
 
 wss.on('connection', ws => {
     console.log("Un client si è connesso a WebSocket");
 
-    // Logga l'ID del client per tracciare meglio chi si connette
     const clientId = Math.random().toString(36).substring(7); // Genera un ID temporaneo per il client
     console.log(`ID Client: ${clientId} - Connessione WebSocket avvenuta`);
 
-    // Gestisce i messaggi in arrivo
     ws.on('message', message => {
         console.log(`ID Client: ${clientId} - Messaggio ricevuto: ${message}`);
         const data = JSON.parse(message);
 
-        // Gestione dell'operazione di unione alla stanza
         if (data.joinRoom) {
             const room = data.joinRoom;
             if (!rooms[room]) {
-                rooms[room] = [];  // Se la stanza non esiste, la creiamo
+                rooms[room] = [];
                 console.log(`ID Client: ${clientId} - Stanza ${room} creata.`);
             }
-            rooms[room].push(ws); // Aggiungi il client alla stanza
+            rooms[room].push(ws);
             console.log(`ID Client: ${clientId} - Client aggiunto alla stanza ${room}. Client tot: ${rooms[room].length}`);
         }
 
-        // Gestisce il comando per avviare il gioco
         if (data.startGame && data.room) {
             console.log(`ID Client: ${clientId} - Tentativo di avviare il gioco nella stanza ${data.room}`);
 
-            // Trova la stanza e invia il messaggio di avvio a tutti i client della stanza
             if (rooms[data.room] && rooms[data.room].length > 0) {
                 console.log(`ID Client: ${clientId} - Stanza trovata, avvio del gioco per ${rooms[data.room].length} client`);
 
@@ -47,11 +42,10 @@ wss.on('connection', ws => {
         }
     });
 
-    // Gestione della chiusura della connessione
     ws.on('close', () => {
         console.log(`ID Client: ${clientId} - Connessione chiusa`);
 
-        // Rimuovi il client dalle stanze
+
         for (let room in rooms) {
             rooms[room] = rooms[room].filter(client => client !== ws);
         }
